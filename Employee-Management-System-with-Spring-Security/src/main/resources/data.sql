3e56bfa8258f6e92f4f9c55dccf8a1b661884e03
Sayan <subhosen16@gmail.com>
2022-07-16 19:34:57 +0530

    Submitting Solution to Rest API assignment

---
+++ Employee-Management-System-with-Spring-Security/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into employee values (1001,'sayan@gmail.com','Sayan','Sengupta');
+insert into employee values (1002,'ankit@gmail.com','Ankit','Sadhu');
+insert into employee values (1003,'payel@gmail.com','Payel','Kumari');
+insert into employee values (1004,'roniket@gmail.com','Roniket','Bandopadhyay');
+insert into employee values (1005,'krish@gmail.com','Sayan','Dutta');
+
+--insert into role values (2,'ADMIN');
+--insert into user values (1,'12345','Sayan');
+--insert into users_roles values (1,2);
